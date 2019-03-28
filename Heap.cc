889283073abc7272adcd0beafb892c5c98aae8bf
jmvallejop1 <jmvallejop1@gmail.com>
2019-03-28 17:11:37 +0100

    Heap implementado pruebalo con el codelite si eso

---
+++ Heap.cc
@@ -0,0 +1,66 @@
+#include "Heap.h"
+
+
+void crearVacio(Heap& h){
+    h.num=0;
+}
+
+void anyadir(Heap& h, ArbolTrie& a){
+    h.num++;
+    h.arboles[h.num]=a;
+    int i = h.num;
+    bool debeSubir;
+
+    if(i>1){
+        debeSubir = obtenerArbolFrecuencia(h.arboles[i]) < obtenerArbolFrecuencia(h.arboles[i/2]);
+    }else{
+        debeSubir = false;
+    }
+
+    while(debeSubir){
+        ArbolTrie aux;
+        aux = h.arboles[i];
+        h.arboles[i]=h.arboles[i/2];
+        h.arboles[i/2]=aux;
+        i = i/2;
+        if(i>1){
+            debeSubir = obtenerArbolFrecuencia(h.arboles[i]) < obtenerArbolFrecuencia(h.arboles[i/2]);
+        }else{
+            debeSubir = false;
+        }
+    }
+}
+
+void eliminarMin(Heap& h){
+    int i,j;
+    ArbolTrie aux;
+    if(h.num>0){
+        h.arboles[0]= h.arboles[h.num];
+        h.num--;
+        i = 1;
+        while(i<(h.num/2)){
+            if(2*i==h.num|obtenerArbolFrecuencia(h.arboles[2*i]) < obtenerArbolFrecuencia(h.arboles[2*i+1])){
+                j= 2*i;
+            }else{
+                j = 2*i+1;
+            }
+
+            if(obtenerArbolFrecuencia(h.arboles[i]) > obtenerArbolFrecuencia(h.arboles[j])){
+                aux = h.arboles[i];
+                h.arboles[i]= h.arboles[j];
+                h.arboles[j]= aux;
+                i = j;
+            }else{
+                i = h.num;
+            }
+        }
+    }
+}
+
+ArbolTrie min(Heap& h){
+    return h.arboles[1];
+}
+
+bool esVacio(Heap& h){
+    return h.num == 0;
+}
---
+++ Heap.h
@@ -0,0 +1,31 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+#include <iterator>
+#include <iostream>
+#include <cstring>
+#include "CarFrec.h"
+#include "ArbolTrie.h"
+
+using namespace std;
+
+struct Heap {
+	private:
+		// vector de tuplas <caracter, frecuencia> ordenado por frecuencias
+		ArbolTrie arboles[257];
+        int num;
+	public:
+	
+	    friend void crearVacio(Heap& h);
+
+        friend void anyadir(Heap& h, ArbolTrie& a);
+
+        friend void eliminarMin(Heap& h);
+
+        friend ArbolTrie min(Heap& h);
+
+        friend bool esVacio(Heap& h);
+	 
+};
+
+#endif
